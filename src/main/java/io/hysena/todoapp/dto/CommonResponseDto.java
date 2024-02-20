997851f120fc6bdf9b90fb22d361b6a74b6e8b05
dbfjru <dbfjru@gmail.com>
2024-02-21 04:30:07 +0900

    Fix : CommonResponseDto @Pattern 불필요

--- src/main/java/io/hysena/todoapp/dto/CommonResponseDto.java
+++ src/main/java/io/hysena/todoapp/dto/CommonResponseDto.java
@@ -11,8 +11,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL) //Json으로 만들 때 NULL 이 아닌 값만 만든다.
 public class CommonResponseDto {
-    @Pattern(regexp = "^[a-z0-9]{4,11}$")
     private String message;
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$")
     private Integer statusCode;
 }
